27ddb41119ad096bb1b9f6c1fc57862f8317ab70
delvis Sanabria <134345133+DelvisSanabria@users.noreply.github.com>
2025-12-03 09:19:48 -0400

    clase Modularizacion

---
+++ Python/especializado/Modularidad/calculadora.py
@@ -0,0 +1,17 @@
+from helpers.herramientas import greet
+from helpers.herramientas import calculate_circle_area as ca
+
+def execute_calculations():
+    print(greet("Bob", 25, "male"))
+    radius = 10
+    area = ca(radius)
+    print(f"The area of a circle with radius {radius} is: {area}")
+
+def sum_numbers(a, b):
+    return a + b
+  
+def subtract_numbers(a, b):
+    return a - b
+
+if __name__ == "__main__":
+  execute_calculations()
